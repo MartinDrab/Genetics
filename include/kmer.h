bbe4e50b3c3e992cbeb3a451e0879f405e361b96
Martin Drab <martin.drab@email.cz>
2017-07-08 01:54:58 +0200

    Adding macros for switching to short kmers

--- include/kmer.h
+++ include/kmer.h
@@ -16,6 +16,8 @@
 #include "kmer-short.h"
 
 
+#if 1
+
 #define KMER_MAXIMUM_SIZE						KMER_DEBUG_MAXIMUM_SIZE
 #define KMER_BYTES(aKMerSize)					KMER_DEBUG_BYTES(aKMerSize)
 #define KMER_BYTES_EXTRA(aKMerSize, aExtra)		KMER_DEBUG_BYTES_EXTRA(aKMerSize, aExtra)
@@ -36,6 +38,30 @@
 #define	kmer_seq_equal					kmer_debug_seq_equal
 #define	kmer_print						kmer_debug_print
 #define	kmer_hash						kmer_debug_hash
+
+#else
+
+#define KMER_MAXIMUM_SIZE						KMER_SHORT_MAXIMUM_SIZE
+#define KMER_BYTES(aKMerSize)					KMER_SHORT_BYTES(aKMerSize)
+#define KMER_BYTES_EXTRA(aKMerSize, aExtra)		KMER_SHORT_BYTES_EXTRA(aKMerSize, aExtra)
+
+#define kmer_get_size				kmer_short_get_size
+#define kmer_set_size				kmer_short_set_size
+#define kmer_get_base				kmer_short_get_base
+#define kmer_set_base				kmer_short_set_base
+#define kmer_get_last_base			kmer_short_get_last_base
+#define kmer_get_number				kmer_short_get_number
+#define kmer_set_number				kmer_short_set_number
+
+#define	kmer_seq_init_raw				kmer_short_seq_init_raw
+#define	kmer_seq_init_by_sequence		kmer_short_seq_init_by_sequence
+#define	kmer_advance					kmer_short_advance
+#define	kmer_back						kmer_short_back
+#define	kmer_seq_equal					kmer_short_seq_equal
+#define	kmer_print						kmer_short_print
+#define	kmer_hash						kmer_short_hash
+
+#endif
 
 
 #include "kmer-base.h"
