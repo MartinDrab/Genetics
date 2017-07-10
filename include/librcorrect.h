d75c1d2b7e1cf5dcab3be2ee90443fc405ceb63f
Martin Drab <martin.drab@email.cz>
2017-07-10 16:21:30 +0200

    Dividing libcorrect routines to make more statistics

--- include/librcorrect.h
+++ include/librcorrect.h
@@ -19,9 +19,18 @@ typedef struct _LIBRCORRECT_STATISTICS {
 	int K;
 } LIBRCORRECT_STATISTICS, *PLIBRCORRECT_STATISTICS;
 
+typedef struct _LIBCORRECT_STATE {
+	PONE_READ Reads;
+	size_t Count;
+	void *ConvertedReads;
+	int K;
+} LIBCORRECT_STATE, *PLIBCORRECT_STATE;
 
-ERR_VALUE libcorrect_correct(PONE_READ Reads, size_t Count, const uint32_t Iterations, PLIBRCORRECT_STATISTICS Stats);
 
+ERR_VALUE libcorrect_state_init(PLIBCORRECT_STATE State, const uint32_t NumberOfIterations, PONE_READ Reads, const size_t Count);
+ERR_VALUE libcorrect_correct(PLIBCORRECT_STATE State);
+ERR_VALUE libcorrect_correct_stats(const LIBCORRECT_STATE *State, PLIBRCORRECT_STATISTICS Stats);
+void librcorrect_state_finit(PLIBCORRECT_STATE State);
 
 
 
